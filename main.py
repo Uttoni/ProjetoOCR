3cf49fb354b82a2e81a241f871b870c0b7e3d6da
GabrielHS <gabrielhenri0012@gmail.com>
2021-12-06 23:39:05 -0300

    add borda no texto

--- main.py
+++ main.py
@@ -5,13 +5,8 @@ from matplotlib import pyplot as plt
 if __name__ == '__main__':
     IMAGE_PATH = 'black friday.jpg'
 
-
     reader = easyocr.Reader(['en'])
     result = reader.readtext(IMAGE_PATH)
-    result
-
-
-
 
     certeza = []
     top_left = []
@@ -31,16 +26,14 @@ if __name__ == '__main__':
     print(certeza)
     font = cv2.FONT_HERSHEY_SIMPLEX
 
-
-
-
-
     img = cv2.imread(IMAGE_PATH)
 
     for i in range(len(palavras)):
         if certeza[i] > 0.5:
-            img = cv2.rectangle(img, top_left[i], bottom_right[i], (0, 255, 0), 3)
-            img = cv2.putText(img, palavras[i], top_left[i], font, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.rectangle(img, top_left[i], bottom_right[i], (0, 255, 0), 3)
+            cv2.putText(img, palavras[i], top_left[i], font, 0.5, (255, 0, 0), 4, cv2.LINE_AA)
+            cv2.putText(img, palavras[i], top_left[i], font, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
+    plt.figure(figsize=(15, 15))
     plt.imshow(img)
     plt.show()
